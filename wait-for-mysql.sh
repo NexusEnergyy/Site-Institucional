f3d3d1996c3933becc72e9c70b038232d0e12b85
Polastrine <diogo.dsilva@sptech.school>
2024-11-05 21:00:27 -0300

    feat: criando sh para rodar o sql depois da criação do java

---
+++ wait-for-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until mysql -h "$host" -u "$DB_USER" -p"$DB_PASSWORD" -e 'SELECT 1'; do
+  >&2 echo "MySQL está indisponível - aguardando..."
+  sleep 1
+done
+
+>&2 echo "MySQL está pronto - executando comando"
+exec $cmd
