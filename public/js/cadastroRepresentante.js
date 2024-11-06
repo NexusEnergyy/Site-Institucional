ddabeac7bdb08245a1a3031a992416160f5c8009
Alisson-ferro <alisson.souza@sptech.school>
2024-11-05 21:43:49 -0300

    Dahsboard sprint 2

--- public/js/cadastroRepresentante.js
+++ public/js/cadastroRepresentante.js
@@ -14,25 +14,69 @@ document.addEventListener("DOMContentLoaded", function () {
   boxesDiv.style.transform = 'translateY(780px)';
 });
 
-function cadastrar_empresa() {
-  const nome_empresa = document.getElementById('input_nome_empresa').value;
-  const submercado = document.getElementById('selectSubMercado').value;
-  const logradouro = document.getElementById('input_logradouro').value;
-  const CNPJ = document.getElementById('input_CNPJ').value;
+function selecionar_categoria(){
+
+  let categoria = document.getElementById('select_Categoria').value;
+  let campos_matriz = document.getElementById('campos_matriz');
   const resposta_erro = document.getElementById('resposta_erro');
 
-  if (nome_empresa !== "" && submercado !== "" && logradouro !== "" && CNPJ !== "") {
-      if (CNPJ.length === 14) {
-          mudarTela1();
-      } else {
-          resposta_erro.innerHTML = `O CNPJ deve ter 14 números`;
-          return false;
-      }
-  } else {
-      resposta_erro.innerHTML = `Preencha todos os campos para continuar`;
-      return false;
+  // Exibe o input somente se a categoria for "matriz"
+  if (categoria == "Matriz") {
+    campos_matriz.style.display = "block";
+    resposta_erro.innerHTML = "";
+  }if (categoria == "Filial") {
+    campos_matriz.style.display = "none";
+    resposta_erro.innerHTML = "";
+  }else{
+    resposta_erro.innerHTML = `Selecione a categoria para que seja preenchidatodas as informaçoes necessárias`;
+     return false;
   }
- }
+}
+
+
+function cadastrar_empresa() {
+
+  const categoria = document.getElementById('select_Categoria').value;
+  const nome_empresa = document.getElementById('input_nome_empresa').value;
+  const submercado = document.getElementById('select_SubMercado').value;
+  const cidade = document.getElementById('input_cidade').value;
+  const uf = document.getElementById('input_UF').value;
+  const CNPJ = document.getElementById('input_CNPJ').value;
+  const resposta_erro = document.getElementById('resposta_erro');
+  const ativoTotal= document.getElementById("input_ativo_total").value;
+
+  if (categoria === "Matriz") {
+    if (ativoTotal === "") {
+      resposta_erro.innerHTML = `Informe o ativo total`;
+      return false;
+    }
+  }
+  if(categoria == "" || nome_empresa == "" || submercado == "" || cidade == "" || uf == "" || CNPJ == ""){
+    resposta_erro.innerHTML = `Preencha todos os campos para continuar`;
+  }
+  if(CNPJ.length == 14){
+    mudarTela1();
+  }
+}
+
+
+ 
+
+  // fetch("/usuarios/cadastrar_empresa", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     categoriaServer:categoria,
+  //     nome_empresaServer: nome_empresa,
+  //     submercadoServer: submercado,
+  //     cidadeServer: cidade,
+  //     ufServer: uf,
+  //     CNPJSrver: CNPJ,
+  //     ativoTotalServer: ativoTotal
+  //   }),
+  // });
 
 // Função para transição de tela
 function mudarTela1() {
@@ -45,13 +89,23 @@ function mudarTela1() {
   const inputNome1 = document.getElementById("input_nome_empresa");
   const inputEmail2 = document.getElementById("selectSubMercado");
 
+ 
+  console.log("boxCinza:", boxCinza);
+  console.log("boxRosa:", boxRosa);
+  console.log("boxRosaContent1:", boxRosaContent1);
+  console.log("boxRosaContent2:", boxRosaContent2);
+  console.log("boxCinzaContent1:", boxCinzaContent1);
+  console.log("boxCinzaContent2:", boxCinzaContent2);
+
   if (boxCinza && boxRosa && boxRosaContent1 && boxRosaContent2 && boxCinzaContent1 && boxCinzaContent2) {
       boxCinza.style.transform = "translateX(100%)";
       boxRosa.style.transform = "translateX(-100%)";
       boxCinza.style.borderRadius = "0 50px 50px 0";
       boxRosa.style.borderRadius = "50px 0 0 50px";
-      inputNome1.value = '';
-      inputEmail2.value = '';
+      
+
+      if (inputNome1) inputNome1.value = '';
+      if (inputEmail2) inputEmail2.value = '';
 
       setTimeout(function () {
           boxRosaContent1.style.display = "none";
@@ -59,13 +113,13 @@ function mudarTela1() {
           boxCinzaContent1.style.display = "none";
           boxCinzaContent2.style.display = "block";
       }, 300); // Tempo em milissegundos
+  } else {
+      console.log("Um ou mais elementos não foram encontrados.");
   }
 }
 
 
-
-
-function CadastrarFuncionarioADM() {
+function cadastrar_adm() {
   var nome_usuario_adm = input_nome_usuario_adm.value;
   var Email = inputEmail.value;
   var filial = input_filial.value;
@@ -81,23 +135,23 @@ function CadastrarFuncionarioADM() {
 
   resposta_erro2.innerHTML="Cadastrado"
 
-  // fetch("/usuarios/cadastrar", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     nome_usuario_admServer: nome_usuario_adm,
-  //     EmailServer: Email,
-  //     filialServer: filial,
-  //     matriculaServer: matricula,
-  //   }),
-  // });
+  fetch("/usuarios/cadastrar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome_usuario_admServer: nome_usuario_adm,
+      EmailServer: Email,
+      filialServer: filial,
+      matriculaServer: matricula,
+    }),
+  });
 
   
 }
 
-document.getElementById('voltarPrimeiraTela').addEventListener('click', () => voltarPrimeiraTela());
+
 
 function voltarPrimeiraTela() {
   const boxCinza = document.querySelector(".boxCinza");
