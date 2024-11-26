b387e76f5409484a248a821b7a95556af5f6504c
Alisson-ferro <alisson.souza@sptech.school>
2024-11-26 19:47:03 -0300

    Alterações nas configuração da dash

--- public/js/NexSystem.js
+++ public/js/NexSystem.js
@@ -130,7 +130,7 @@ function fecharModal() {
   const modal = document.getElementById('modal');
   modal.classList.remove('show');
   modal.classList.add('hide');
-  modal.closeModal()
+  modal.close();
 }
 
 
